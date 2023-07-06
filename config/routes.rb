f7fd2b32421d125d8e27cd41862193fad84a9576
okjm3324 <okjm.yuma@gmail.com>
2023-07-06 21:02:40 +0900

    Fix:ルーティング、READMEの修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resource :user_session, only: [:new, :create, :destroy]
+  resource :user_sessions, only: [:new, :create, :destroy]
   # Defines the root path route ("/")
   # root "articles#index"
 end
