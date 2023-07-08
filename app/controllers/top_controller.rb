0b8520c8fb5de910206e0e041c7425bdd28ca6c8
okjm3324 <okjm.yuma@gmail.com>
2023-07-08 16:34:11 +0900

    Add: ログイン機能を追加
    ・ログインフォームの追加
    ・ログイン機能を追加
    ・user_sessions_controllerを追加
    ・フラッシュメッセージのパーシャルを追加

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,5 @@
 class TopController < ApplicationController
+  skip_before_action :require_login, only: %i[top] 
   def top;end
   def index
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
 
+
   root "top#top"
 
 
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resource :user_sessions, only: [:new, :create, :destroy]
+  get    'login',  to: 'user_sessions#new'
+  post   'login',  to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
   # Defines the root path route ("/")
   # root "articles#index"
 end
