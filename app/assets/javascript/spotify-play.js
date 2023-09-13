93faccc092addaf11cb388f1cd18f3d61fe76b5a
okjm3324 <okjm.yuma@gmail.com>
2023-09-13 21:12:09 +0900

    Add: AB区間再生機能
    AB区間再生機能を実装
    モデルのリレーションを追記

--- app/assets/javascript/spotify-play.js
+++ app/assets/javascript/spotify-play.js
@@ -2,10 +2,15 @@ document.addEventListener('DOMContentLoaded', () => {
   let player;
   let device_id;
   let isTrackLoaded = false;
+  let isPaused = false; 
   const startTime = 30;
-  
+  const endTime = 35; 
+
   const access_token = document.getElementById('access-token-input').value; 
   const track_id = document.getElementById('song-code-input').value; 
+
+  let timer;
+  let pauseTime; // 追加
 
   window.onSpotifyWebPlaybackSDKReady = () => {
     player = new Spotify.Player({
@@ -21,6 +26,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     player.addListener('player_state_changed', state => {
       console.log('Player state changed', state);
+      if (state) {
+        isPaused = state.paused;
+        if (isPaused) {
+          pauseTime = state.position; // 一時停止時の位置を記録
+        }
+      }
     });
 
     player.addListener('initialization_error', ({ message }) => {
@@ -30,10 +41,23 @@ document.addEventListener('DOMContentLoaded', () => {
     player.connect();
   };
 
+  function setLoopTimeout() {
+    let playDuration = (endTime * 1000) - pauseTime; // 経過時間を考慮した再生時間を計算
+    timer = setTimeout(() => {
+      player.pause().then(() => {
+        console.log('Paused at B position');
+        player.seek(startTime * 1000).then(() => {
+          console.log(`Reset to A position: ${startTime}s`);
+        });
+      });
+    }, playDuration);
+    pauseTime = 0; // タイマーをリセット
+  }
+
   function loadTrack() {
     fetch(`https://api.spotify.com/v1/me/player/play?device_id=${device_id}`, {
       method: 'PUT',
-      body: JSON.stringify({ uris: [`spotify:track:${track_id}`],  position_ms: startTime * 1000}),
+      body: JSON.stringify({ uris: [`spotify:track:${track_id}`], position_ms: startTime * 1000 }),
       headers: {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${access_token}`
@@ -41,6 +65,8 @@ document.addEventListener('DOMContentLoaded', () => {
     }).then(() => {
       console.log('Track loaded');
       isTrackLoaded = true;
+      pauseTime = startTime * 1000; // pauseTimeを初期化
+      setLoopTimeout();
     }).catch((error) => {
       console.error('Failed to load track', error);
     });
@@ -52,6 +78,10 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
       player.togglePlay().then(() => {
         console.log('Toggled play/pause');
+        clearTimeout(timer);
+        if (isPaused) {
+          setLoopTimeout();
+        }
       });
     }
   });
